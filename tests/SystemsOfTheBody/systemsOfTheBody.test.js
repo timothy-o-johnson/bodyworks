ef90cca756ff894081f8b31246f2488e0a4ce663
Timothy Johnson <tjohnson@2ndswing.com>
2022-08-19 07:42:02 -0400

    refactor: formatting

--- tests/SystemsOfTheBody/systemsOfTheBody.test.js
+++ tests/SystemsOfTheBody/systemsOfTheBody.test.js
@@ -8,7 +8,6 @@ describe('0. The Body ', () => {
 
 describe('1. Systems of The Body', () => {
   it(`a. the skeletal system should consist of bones and and ligaments that secure the bones at joints`, () => {
-
     expect(theBody.skeletalSystem).toContain('bones')
     expect(theBody.skeletalSystem).toContain('ligaments')
   })
@@ -19,34 +18,58 @@ describe('1. Systems of The Body', () => {
   })
 
   it('c. the muscular system includes: skeletal muscles, cardiac muscle, smooth muscle', () => {
-
-    const muscularSystemMuscles = ['skeletal muscles', 'cardiac muscle', 'smooth muscle']
+    const muscularSystemMuscles = [
+      'skeletal muscles',
+      'cardiac muscle',
+      'smooth muscle'
+    ]
 
     muscularSystemMuscles.forEach(muscle => {
-
       expect(theBody.muscularSystem).toContain(muscle)
     })
   })
 
-  it('d. the cardiovascular system includes: four-chambered heart, arteries, capillaries, veins', ()=>{
-    const cardiovascularSystem = ['four-chambered heart', 'arteries', 'capillaries', 'veins']
+  it('d. the cardiovascular system includes: four-chambered heart, arteries, capillaries, veins', () => {
+    const cardiovascularSystem = [
+      'four-chambered heart',
+      'arteries',
+      'capillaries',
+      'veins'
+    ]
 
     cardiovascularSystem.forEach(part => {
-
       expect(theBody.cardiovascularSystem).toContain(part)
     })
   })
 
-  it('e. the lymphatic system includes vessels and nodes', ()=>{
+  it('e. the lymphatic system includes vessels and nodes', () => {
+    // recovers bodies tissue flueds and returns them to the heart
     const lymphaticSystem = ['vessels', 'nodes']
 
     lymphaticSystem.forEach(part => {
-
       expect(theBody.lymphaticSystem).toContain(part)
     })
   })
 
+  it('f. the nervous system includes the central nervous system and the peripheral nervous system', () => {
+    // recovers bodies tissue flueds and returns them to the heart
+    const nervousSystem = [
+      { centralNervousSystem: ['brain', 'spinal cord'] },
+      {
+        peripheralNervousSystem: [
+          'nerves',
+          {
+            visceralNervousSystem:
+              'involuntary flight-or-fight and vegetative functions'
+          }
+        ]
+      }
+    ]
 
+    nervousSystem.forEach(part => {
+      expect(theBody.nervousSystem).toContain(part)
+    })
+  })
 })
 
 // testing
--- theBody.js
+++ theBody.js
@@ -1,11 +1,28 @@
 const body = {
-    articularSystem: ['fixed joints', 'moveable joints'],
-    cardiovascularSystem: ['four-chambered heart', 'arteries', 'capillaries', 'veins'],
-    lymphaticSystem : ['vessels', 'nodes'],
-    muscularSystem: ['skeletal muscles', 'cardiac muscle', 'smooth muscle'],
-    skeletalSystem: ['bones', 'ligaments'],
+  articularSystem: ['fixed joints', 'moveable joints'],
+  cardiovascularSystem: [
+    'four-chambered heart',
+    'arteries',
+    'capillaries',
+    'veins'
+  ],
+  lymphaticSystem: ['vessels', 'nodes'],
+  muscularSystem: ['skeletal muscles', 'cardiac muscle', 'smooth muscle'],
+  nervousSystem: [
+    { centralNervousSystem: ['brain', 'spinal cord'] },
+    {
+      peripheralNervousSystem: [
+        'nerves',
+        {
+          visceralNervousSystem:
+            'involuntary flight-or-fight and vegetative functions'
+        }
+      ]
+    }
+  ],
+  skeletalSystem: ['bones', 'ligaments']
 }
 
 module.exports = {
-    body
+  body
 }
